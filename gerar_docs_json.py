84642f03c12702754c8e2d221a5c52ed344a9478
João Treuk <joaotreuk@outlook.com>
2023-03-21 12:07:25 -0300

    build: script p/ gerar o docs.json automaticamente

---
+++ gerar_docs_json.py
@@ -0,0 +1,41 @@
+import locale
+import json
+import os
+
+# Define a localização para Português do Brasil
+locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+
+# Obter objeto de estrutura da pasta
+def get_directory_structure(pasta):
+  estrutura = []
+  for item in sorted(os.listdir(pasta), key = locale.strxfrm):
+    caminho = os.path.join(pasta, item)
+    
+    if os.path.isdir(caminho):
+      objeto = {
+        "name": item,
+        "children": get_directory_structure(caminho)
+      }
+
+      # Definições c#
+      if item == "cSharp":
+        objeto["id"] = item
+        objeto["fileExtension"] = "cs"
+        objeto["name"] = "c#"
+
+      estrutura.append(objeto)
+    elif os.path.isfile(caminho):
+      # Salva diferente os arquivos xlsx
+      partesNome = os.path.splitext(item)
+      if partesNome[1] == ".xlsx":
+        estrutura.append({
+          "name": "Tipo de Dados",
+          "file": item
+        })
+      else:
+        estrutura.append(partesNome[0])
+  return estrutura
+
+# Criar o arquivo JSON de documentos
+with open("public\\docs.json", "w") as arquivo:
+  arquivo.write(json.dumps(get_directory_structure("public\\docs"), indent = 2))
